ed38bca44e6199b74b6fcdb7f148e1c4a3dca6e6
Appokaliptik <alexanderstrannik@gmail.com>
2018-08-06 19:35:17 +0300

    js update

--- JavaScript/js/common.js
+++ JavaScript/js/common.js
@@ -1,24 +1,33 @@
 (function () {
-
   
   function sum(a) {
-    var currentSum = 0;
+    var currentSum;
+    if (a !== undefined) {
+      currentSum = a;
+    } else {
+      currentSum = 0;
+    }
 
     function subSum(b){
       if (b !== undefined) {
         currentSum += b;
-      }
-      return subSum;
+        return subSum;
+      } else {
+        return currentSum;
+      }      
     }
     subSum.toString = function() {
       return currentSum;
     };    
-    return subSum;    
+    return subSum;
   }
   
-  console.log(sum(0)(5)(3)(8)(-2));
-  console.log(sum(0)(5)(3)(8)());
+  console.log(sum(5)());
+  console.log(sum(5)(6)());
   console.log(sum());
+  console.log(sum(1)() === 1);
+  console.log(sum(5)().toFixed(16));
+  
 
 
 }());
