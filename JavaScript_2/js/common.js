fcb53cb3988e75857e3773482a36e76d6dc24d82
Appokaliptik <alexanderstrannik@gmail.com>
2018-08-16 17:23:10 +0300

    update js2

--- JavaScript_2/js/common.js
+++ JavaScript_2/js/common.js
@@ -1,16 +1,16 @@
 (function () {
 
   var fn1 = () => {
-    console.log('fn1')
-    return Promise.resolve(1)
+    console.log('fn1');
+    return Promise.resolve(2);
   }
 
   var fn2 = () => new Promise(resolve => {
     console.log('fn2')
-    setTimeout(() => resolve(2), 1000)
+    setTimeout(() => resolve(4), 1000)
   })
 
-  function promiseReduce(asyncFunctions, reduce, initialValue) {     
+  function promiseReduce(asyncFunctions, reduce, initialValue) {
     return new Promise((resolve) =>{      
       var length = asyncFunctions.length;
       var results = new Array(length);
@@ -18,25 +18,21 @@
       if (initialValue == undefined) {
         initialValue = 0;
       }
-      for (var i = initialValue; i <= length; i++) {
-        reduceResult += results[i];
-      }
-      result = reduceResult
-      resolve( result );
-     
+      for (var i = 0; i < length; i++) {
+        asyncFunctions[i]().then(result => {
+          initialValue = initialValue * result 
+          console.log(initialValue);
+        }) ;
+      }      
     })
-    .then(
-      result=>{
-        console.log(result);
-      }
-    )
+    
   }
 
   promiseReduce(
     [fn1, fn2], 
     function (memo, value) {
       console.log('reduce');
-      return memo + value;
+      return memo * value;
     }, 
     1
     )
